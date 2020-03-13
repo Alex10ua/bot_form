e1d96687df626fad8c36566e0b5c6a8bd5db4e4a
Alex <sasha.lazoriak@gmail.com>
2020-03-13 01:53:54 +0100

    [add] Added moc file where need to change a data

---
+++ Data1.py
@@ -0,0 +1,9 @@
+nameandlastname ='Твоє ім я'
+numberroom ='кімната'
+partofrandomtemperature = '36.'
+discription ='твій опис'
+URL = 'https://forms.office.com/Pages/ResponsePage.aspx?id=7cGw-B1o6UaMK4vw2W0OXvcyybhA5aZMsi7Eb-uAd4pUNFE0SjQ0Uk5PNjFZVzVaWFZRRjNIM0I5Mi4u'
+# Потрібно клас Data1 перейменувати в Data і підкладіть свої дані
+
+# В класі Programsleep.py змініть години. перший запуск відправить зразу форму але потом буде по годинам але це чисто тереотично
+
