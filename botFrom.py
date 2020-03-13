a1f84753a583c5d212e1dd8f7637e5a78832f036
Alex <sasha.lazoriak@gmail.com>
2020-03-13 01:40:43 +0100

    [add] added options for chrome and set sleep

--- botFrom.py
+++ botFrom.py
@@ -1,6 +1,7 @@
+import time
+
 from selenium import webdriver
-from time import sleep
-import schedule
+from selenium.webdriver.chrome.options import Options
 
 import random
 import Data
@@ -12,32 +13,39 @@ class FormBot():
         self.driver.get(Data.URL)
 
     def __init__(self):
-        self.driver = webdriver.Chrome('chromedriver.exe')
+        options = Options()
+        options.add_argument("start-maximized")
+        self.driver = webdriver.Chrome(chrome_options=options, executable_path='chromedriver.exe')
 
     def randomtemperature(self):
-        generatedvalue=random.randint(3, 8)
+        generatedvalue = random.randint(3, 8)
         return generatedvalue
+
     def randomStar(self):
         return random.randint(1, 2)
+
     def fillin(self):
-        fieldnamelastname= self.driver.find_element_by_xpath('/html/body/div/div/div/div/div[1]/div/div[1]/div[2]/div[2]/div[1]/div[2]/div[3]/div/div/div/input')
+        fieldnamelastname = self.driver.find_element_by_xpath('/html/body/div/div/div/div/div[1]/div/div[1]/div[2]/div[2]/div[1]/div[2]/div[3]/div/div/div/input')
         fieldnamelastname.send_keys(Data.nameandlastname)
-        fieldnumberroom= self.driver.find_element_by_xpath('/html/body/div/div/div/div/div[1]/div/div[1]/div[2]/div[2]/div[2]/div[2]/div[3]/div/div/div/input')
+        fieldnumberroom = self.driver.find_element_by_xpath('/html/body/div/div/div/div/div[1]/div/div[1]/div[2]/div[2]/div[2]/div[2]/div[3]/div/div/div/input')
         fieldnumberroom.send_keys(Data.numberroom)
-        fieldoftemperature=self.driver.find_element_by_xpath('/html/body/div/div/div/div/div[1]/div/div[1]/div[2]/div[2]/div[3]/div[2]/div[3]/div/div/div/input')
+        fieldoftemperature = self.driver.find_element_by_xpath('/html/body/div/div/div/div/div[1]/div/div[1]/div[2]/div[2]/div[3]/div[2]/div[3]/div/div/div/input')
         fieldoftemperature.send_keys(Data.partofrandomtemperature+str(self.randomtemperature()))
         if(self.randomStar()==2):
             fieldrate = self.driver.find_element_by_xpath(
                 '/html/body/div/div/div/div/div[1]/div/div[1]/div[2]/div[2]/div[4]/div[2]/div[3]/div/div/div/div/div[1]/span[5]')
             fieldrate.click()
         else:
-            fieldrate1=self.driver.find_element_by_xpath('/html/body/div/div/div/div/div[1]/div/div[1]/div[2]/div[2]/div[4]/div[2]/div[3]/div/div/div/div/div[1]/span[4]')
+            fieldrate1 = self.driver.find_element_by_xpath('/html/body/div/div/div/div/div[1]/div/div[1]/div[2]/div[2]/div[4]/div[2]/div[3]/div/div/div/div/div[1]/span[4]')
             fieldrate1.click()
 
-        fielddescriptin=self.driver.find_element_by_xpath('/html/body/div/div/div/div/div[1]/div/div[1]/div[2]/div[2]/div[5]/div[2]/div[3]/div/div/div/textarea')
+        fielddescriptin = self.driver.find_element_by_xpath('/html/body/div/div/div/div/div[1]/div/div[1]/div[2]/div[2]/div[5]/div[2]/div[3]/div/div/div/textarea')
         fielddescriptin.send_keys(Data.discription)
-        buttontsend=self.driver.find_element_by_xpath('/html/body/div/div/div/div/div[1]/div/div[1]/div[2]/div[3]/div/button/div')
+        buttontsend = self.driver.find_element_by_xpath('/html/body/div/div/div/div/div[1]/div/div[1]/div[2]/div[3]/div/button/div')
+        time.sleep(random.randint(60, 300))
         buttontsend.click()
+        time.sleep(15)
+
 
 bot = FormBot()
 bot.login()
