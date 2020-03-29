845ded84e381bcbf3ead758a1ef4e23ab77001f9
Alex <sasha.lazoriak@gmail.com>
2020-03-29 23:52:55 +0200

    [change] Changed some data

--- Programsleep.py
+++ Programsleep.py
@@ -4,7 +4,7 @@ import botFrom
 
 
 def job():
-    bot=botFrom.FormBot()
+    bot = botFrom.FormBot()
     bot.login()
     bot.fillin()
    # botFrom.bot.login()
@@ -14,7 +14,7 @@ def job():
 #schedule.every().minute.at(":17").do(job)
 #schedule.every().day.at("10:30").do(job)
 schedule.every().day.at("07:32").do(job)
-schedule.every().day.at("20:18").do(job)
+schedule.every().day.at("20:00").do(job)
 
 while True:
     schedule.run_pending()
